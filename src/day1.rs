3db1c9d21aa6cde0567f03077ee4c0e742d2f279
clubby789 <jamie@hill-daniel.co.uk>
2023-12-01 22:22:56 +0000

    day1 part 2: use bytes only to check for ascii digits

--- src/day1.rs
+++ src/day1.rs
@@ -22,20 +22,20 @@ pub fn part2() -> usize {
         .lines()
         .map(|l| {
             fn num(s: &str) -> Option<usize> {
-                static NUMS: [(&str, &str); 9] = [
-                    ("1", "one"),
-                    ("2", "two"),
-                    ("3", "three"),
-                    ("4", "four"),
-                    ("5", "five"),
-                    ("6", "six"),
-                    ("7", "seven"),
-                    ("8", "eight"),
-                    ("9", "nine"),
+                static NUMS: [(u8, &str); 9] = [
+                    (b'1', "one"),
+                    (b'2', "two"),
+                    (b'3', "three"),
+                    (b'4', "four"),
+                    (b'5', "five"),
+                    (b'6', "six"),
+                    (b'7', "seven"),
+                    (b'8', "eight"),
+                    (b'9', "nine"),
                 ];
                 NUMS.iter()
                     .enumerate()
-                    .find(|(_, &(n1, n2))| s.starts_with(n1) || s.starts_with(n2))
+                    .find(|(_, &(n1, n2))| s.as_bytes().get(0).copied() == Some(n1) || s.starts_with(n2))
                     .map(|(i, _)| i + 1)
             }
             let n1 = (0..l.len()).find_map(|n| num(&l[n..])).unwrap();

919f641ff4caced3f08cfe12a0e86f974acf19ca
clubby789 <jamie@hill-daniel.co.uk>
2023-12-03 01:45:13 +0000

    day1 part2: Custom searching iterator

--- src/day1.rs
+++ src/day1.rs
@@ -5,10 +5,15 @@ pub fn part1() -> usize {
         .lines()
         .map(|l| {
             (
-                l.as_bytes().iter().find(|b| b.is_ascii_digit())
+                l.as_bytes()
+                    .iter()
+                    .find(|b| b.is_ascii_digit())
                     .map(|i| i - b'0')
                     .unwrap(),
-                l.as_bytes().iter().rev().find(|b| b.is_ascii_digit())
+                l.as_bytes()
+                    .iter()
+                    .rev()
+                    .find(|b| b.is_ascii_digit())
                     .map(|i| i - b'0')
                     .unwrap(),
             )
@@ -21,26 +26,76 @@ pub fn part2() -> usize {
     INPUT
         .lines()
         .map(|l| {
-            fn num(s: &str) -> Option<usize> {
-                static NUMS: [(u8, &str); 9] = [
-                    (b'1', "one"),
-                    (b'2', "two"),
-                    (b'3', "three"),
-                    (b'4', "four"),
-                    (b'5', "five"),
-                    (b'6', "six"),
-                    (b'7', "seven"),
-                    (b'8', "eight"),
-                    (b'9', "nine"),
-                ];
-                NUMS.iter()
-                    .enumerate()
-                    .find(|(_, &(n1, n2))| s.as_bytes().get(0).copied() == Some(n1) || s.starts_with(n2))
-                    .map(|(i, _)| i + 1)
-            }
-            let n1 = (0..l.len()).find_map(|n| num(&l[n..])).unwrap();
-            let n2 = (0..=l.len()).rev().find_map(|n| num(&l[n..])).unwrap();
-            n1 * 10 + n2
+            let matcher = |window: &[u8]| match window {
+                &[n, ..] if n.is_ascii_digit() => Some((n - b'0') as usize),
+                &[b'o', b'n', b'e', ..] => Some(1),
+                &[b't', b'w', b'o', ..] => Some(2),
+                &[b't', b'h', b'r', b'e', b'e', ..] => Some(3),
+                &[b'f', b'o', b'u', b'r', ..] => Some(4),
+                &[b'f', b'i', b'v', b'e', ..] => Some(5),
+                &[b's', b'i', b'x', ..] => Some(6),
+                &[b's', b'e', b'v', b'e', b'n', ..] => Some(7),
+                &[b'e', b'i', b'g', b'h', b't', ..] => Some(8),
+                &[b'n', b'i', b'n', b'e', ..] => Some(9),
+                &[_, _, _, _, _, _, ..] => unreachable!(),
+                _ => None,
+            };
+            let hi = WindowsUpto::new(l.as_bytes(), 5).find_map(matcher).unwrap();
+            let lo = WindowsUpto::new(l.as_bytes(), 5)
+                .rev()
+                .find_map(matcher)
+                .unwrap();
+            hi * 10 + lo
         })
         .sum()
+}
+
+struct WindowsUpto<'a, T: 'a> {
+    v: &'a [T],
+    size: usize,
+    steps_back: usize,
+}
+
+impl<'a, T: 'a> WindowsUpto<'a, T> {
+    pub fn new(v: &'a [T], size: usize) -> Self {
+        Self {
+            v,
+            size,
+            steps_back: 0,
+        }
+    }
+}
+
+impl<'a, T> Iterator for WindowsUpto<'a, T> {
+    type Item = &'a [T];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.v.is_empty() {
+            None
+        } else {
+            let length = self.size.min(self.v.len());
+            let ret = Some(&self.v[..length]);
+            self.v = &self.v[1..];
+            ret
+        }
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for WindowsUpto<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.v.is_empty() {
+            None
+        } else {
+            let ret = if self.steps_back < self.size {
+                // first few iterations
+                &self.v[self.v.len() - (self.steps_back + 1)..]
+            } else {
+                &self.v[self.v.len() - (self.steps_back + 1)..][..self.size]
+            };
+            // This should really remove the back of v, but since we only step
+            // exclusively forward or back, who cares :^)
+            self.steps_back += 1;
+            Some(ret)
+        }
+    }
 }

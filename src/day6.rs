f19eb82790779bff6b6f6b6699919d28b8e3d76d
clubby789 <jamie@hill-daniel.co.uk>
2023-12-06 14:00:20 +0000

    day 6 part 2: optimise timing more
    Solved part 2 in 28ns

--- src/day6.rs
+++ src/day6.rs
@@ -1,6 +1,6 @@
 const INPUT: &str = include_str!("inputs/6.txt");
 
-fn calculate_number_of_wins(limit: u32, distance: u32) -> u32 {
+fn calculate_number_of_wins(limit: u64, distance: u64) -> u32 {
     /*
     d = h * (l - h)
     h * (l - h) > d
@@ -16,17 +16,10 @@ fn calculate_number_of_wins(limit: u32, distance: u32) -> u32 {
     (hi.ceil() - lo.floor()) as u32 - 1
 }
 
-fn parse_ascii(s: &str) -> u32 {
+fn parse_ascii(s: &str) -> u64 {
     s.as_bytes()
         .iter()
-        .fold(0, |acc, b| acc * 10 + (b & 0xf) as u32)
-}
-
-fn parse_ascii_skip_spaces(s: &str) -> u32 {
-    s.as_bytes()
-        .iter()
-        .filter(|&&b| b != b' ')
-        .fold(0, |acc, b| acc * 10 + (b & 0xf) as u32)
+        .fold(0, |acc, b| acc * 10 + (b & 0xf) as u64)
 }
 
 pub fn part1() -> usize {
@@ -40,11 +33,42 @@ pub fn part1() -> usize {
         .map(|(time, distance)| calculate_number_of_wins(time, distance) as usize)
         .product()
 }
+
+fn parse_part2(input: &str) -> (u64, u64) {
+    let mut pos = 9;
+    let input = input.as_bytes();
+    while input[pos] == b' ' {
+        pos += 1;
+    }
+    let mut time = 0;
+    loop {
+        match input[pos] {
+            b' ' => (),
+            b'\n' => {
+                break;
+            }
+            n => {
+                debug_assert!(matches!(n, b'0'..=b'9'));
+                time = time * 10 + (n & 0xf) as u64;
+            }
+        }
+        pos += 1;
+    }
+    pos += 10;
+    let mut distance = 0;
+    while let Some(&n) = input.get(pos) {
+        pos += 1;
+        if n == b' ' {
+            continue;
+        }
+        debug_assert!(matches!(n, b'0'..=b'9'), "'{}'", n as char);
+        distance = distance * 10 + (n & 0xf) as u64;
+    }
+
+    (time, distance)
+}
+
 pub fn part2() -> usize {
-    let (times, distances) = INPUT.split_once('\n').unwrap();
-    let times = times.split_once(':').unwrap().1.trim();
-    let distances = distances.split_once(':').unwrap().1.trim();
-    let time = parse_ascii_skip_spaces(times);
-    let distance = parse_ascii_skip_spaces(distances);
+    let (time, distance) = parse_part2(INPUT);
     calculate_number_of_wins(time, distance) as usize
 }

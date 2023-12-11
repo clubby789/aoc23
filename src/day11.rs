f30672cbabc42ee9fb60a573f7ef82de79336833
clubby789 <jamie@hill-daniel.co.uk>
2023-12-11 15:46:50 +0000

    day 11: refactor shared code

--- src/day11.rs
+++ src/day11.rs
@@ -15,59 +15,54 @@ use std::num::NonZeroUsize;
 
 const INPUT: &str = include_str!("inputs/11.txt");
 
-pub fn part1() -> usize {
-    let width =
-        NonZeroUsize::new(INPUT.as_bytes().iter().position(|&b| b == b'\n').unwrap() + 1).unwrap();
-    let mut populated_rows = FxHashSet::default();
-    let mut populated_cols = FxHashSet::default();
-    let galaxies: Vec<_> = INPUT
-        .bytes()
-        .enumerate()
-        .filter(|&(_, b)| b == b'#')
-        .map(|(i, _)| (i % width, i / width))
-        .collect();
-    for &(x, y) in galaxies.iter() {
-        populated_rows.insert(y);
-        populated_cols.insert(x);
-    }
-    let mut sum = 0;
-    for (i, &from) in galaxies.iter().enumerate() {
-        for (j, &to) in galaxies.iter().enumerate().skip(i) {
-            if i == j {
-                continue;
-            }
-            let mut path = 0;
-            let (start_x, end_x) = if from.0 < to.0 {
-                (from.0, to.0)
-            } else {
-                (to.0, from.0)
-            };
-            for x in start_x..end_x {
-                if !populated_cols.contains(&x) {
-                    path += 2
-                } else {
-                    path += 1
-                }
-            }
-
-            let (start_y, end_y) = if from.1 < to.1 {
-                (from.1, to.1)
-            } else {
-                (to.1, from.1)
-            };
-            for y in start_y..end_y {
-                if !populated_rows.contains(&y) {
-                    path += 2
-                } else {
-                    path += 1
-                }
-            }
-            sum += path;
-        }
-    }
-    sum
+struct Space {
+    galaxies: Vec<(usize, usize)>,
+    populated_rows: FxHashSet<usize>,
+    populated_cols: FxHashSet<usize>,
 }
-pub fn part2() -> usize {
+
+impl Space {
+    pub fn path_count<const GROWTH: usize>(&self) -> usize {
+        let mut sum = 0;
+        for (i, &from) in self.galaxies.iter().enumerate() {
+            for (j, &to) in self.galaxies.iter().enumerate().skip(i) {
+                if i == j {
+                    continue;
+                }
+                let mut path = 0;
+                let (start_x, end_x) = if from.0 < to.0 {
+                    (from.0, to.0)
+                } else {
+                    (to.0, from.0)
+                };
+                for x in start_x..end_x {
+                    if !self.populated_cols.contains(&x) {
+                        path += GROWTH
+                    } else {
+                        path += 1
+                    }
+                }
+
+                let (start_y, end_y) = if from.1 < to.1 {
+                    (from.1, to.1)
+                } else {
+                    (to.1, from.1)
+                };
+                for y in start_y..end_y {
+                    if !self.populated_rows.contains(&y) {
+                        path += GROWTH
+                    } else {
+                        path += 1
+                    }
+                }
+                sum += path;
+            }
+        }
+        sum
+    }
+}
+
+fn parse_input(src: &str) -> Space {
     let width =
         NonZeroUsize::new(INPUT.as_bytes().iter().position(|&b| b == b'\n').unwrap() + 1).unwrap();
     let mut populated_rows = FxHashSet::default();
@@ -82,40 +77,19 @@ pub fn part2() -> usize {
         populated_rows.insert(y);
         populated_cols.insert(x);
     }
-    let mut sum = 0;
-    for (i, &from) in galaxies.iter().enumerate() {
-        for (j, &to) in galaxies.iter().enumerate().skip(i) {
-            if i == j {
-                continue;
-            }
-            let mut path = 0;
-            let (start_x, end_x) = if from.0 < to.0 {
-                (from.0, to.0)
-            } else {
-                (to.0, from.0)
-            };
-            for x in start_x..end_x {
-                if !populated_cols.contains(&x) {
-                    path += 1000000
-                } else {
-                    path += 1
-                }
-            }
-
-            let (start_y, end_y) = if from.1 < to.1 {
-                (from.1, to.1)
-            } else {
-                (to.1, from.1)
-            };
-            for y in start_y..end_y {
-                if !populated_rows.contains(&y) {
-                    path += 1000000
-                } else {
-                    path += 1
-                }
-            }
-            sum += path;
-        }
+    Space {
+        galaxies,
+        populated_cols,
+        populated_rows,
     }
-    sum
+}
+
+pub fn part1() -> usize {
+    let space = parse_input(INPUT);
+    space.path_count::<2>()
+}
+
+pub fn part2() -> usize {
+    let space = parse_input(INPUT);
+    space.path_count::<1000000>()
 }

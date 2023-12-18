844c52563e2d778d015dd8232decebd077c26732
clubby789 <jamie@hill-daniel.co.uk>
2023-12-18 16:50:33 +0000

    day 16: tune parsing more
    Solved part 1 in 4.934µs
    Solved part 2 in 6.126µs

--- src/day18.rs
+++ src/day18.rs
@@ -9,7 +9,7 @@ fn solve_points(path: &[(i64, i64)], points_on_path: i64) -> usize {
 
         area += (v1.0 * v2.1) - (v1.1 * v2.0);
     }
-    //let area = area.abs() / 2;
+    // let area = area.abs() / 2;
     // A = i + b/2 - 1
     // A + 1 = i + b/2
     let b_2 = points_on_path / 2;
@@ -19,17 +19,18 @@ fn solve_points(path: &[(i64, i64)], points_on_path: i64) -> usize {
 }
 
 pub fn part1() -> usize {
-    let mut path = Vec::with_capacity(INPUT.len() / 14 + 1);
+    let mut path = Vec::with_capacity(INPUT.len() / 8);
     let mut points_on_path = 0;
     path.push((0, 0));
-    for (direction, amnt) in INPUT.as_bytes().split(|&b| b == b'\n').map(|line| {
-        let (dir, amnt) = match line {
-            &[dir, _, amnt, b' ', ..] => (dir, amnt & 0b1111),
-            &[dir, _, hi, lo, ..] => (dir, (hi & 0b1111) * 10 + (lo & 0b1111)),
+    let input = INPUT.as_bytes();
+    let mut position = 0;
+    while let Some(line) = input.get(position..) {
+        let (direction, amnt, length) = match line {
+            &[dir, _, amnt, b' ', ..] => (dir, (amnt & 0b1111) as i64, 14),
+            &[dir, _, hi, lo, ..] => (dir, ((hi & 0b1111) * 10 + (lo & 0b1111)) as i64, 15),
             _ => unreachable!(),
         };
-        (dir, amnt as i64)
-    }) {
+        position += length;
         let last = *path.last().unwrap();
         let diff = match direction {
             b'U' => (0, -amnt),
@@ -48,46 +49,56 @@ pub fn part2() -> usize {
     let mut path = Vec::with_capacity(INPUT.len() / 14 + 1);
     let mut points_on_path = 0;
     path.push((0, 0));
-    for (direction, amnt) in INPUT.as_bytes().split(|&b| b == b'\n').map(|line| {
+    let input = INPUT.as_bytes();
+    let mut position = 0;
+    while let Some(line) = input.get(position..) {
         fn parse_hex(dir: u8, amnt: [u8; 5]) -> (u8, i64) {
-            let dir = match dir {
-                b'0' => b'R',
-                b'1' => b'D',
-                b'2' => b'L',
-                b'3' => b'U',
-                _ => unreachable!(),
-            };
-            let amnt = amnt.into_iter().fold(0i64, |acc, x| {
-                (acc << 4)
-                    + match x {
-                        b'0'..=b'9' => (x & 0b1111) as i64,
-                        b'a'..=b'f' => (x - b'a' + 10) as i64,
-                        _ => unreachable!(),
-                    }
-            });
+            const fn hex_table() -> [u8; 255] {
+                let mut table = [0; 255];
+                table[b'0' as usize] = 0;
+                table[b'1' as usize] = 1;
+                table[b'2' as usize] = 2;
+                table[b'3' as usize] = 3;
+                table[b'4' as usize] = 4;
+                table[b'5' as usize] = 5;
+                table[b'6' as usize] = 6;
+                table[b'7' as usize] = 7;
+                table[b'8' as usize] = 8;
+                table[b'9' as usize] = 9;
+                table[b'a' as usize] = 10;
+                table[b'b' as usize] = 11;
+                table[b'c' as usize] = 12;
+                table[b'd' as usize] = 13;
+                table[b'e' as usize] = 14;
+                table[b'f' as usize] = 15;
+                table
+            }
+            const HEX_TABLE: [u8; 255] = hex_table();
+            let amnt = amnt
+                .into_iter()
+                .fold(0i64, |acc, x| (acc << 4) + HEX_TABLE[x as usize] as i64);
             (dir, amnt)
         }
-        let (dir, amnt) = match line {
-            &[.., a, b, c, d, e, dir, _] => {
-                parse_hex(dir, [a, b, c, d, e])
+        let ((direction, amnt), len) = match line {
+            &[_, b' ', _, b' ', b'(', b'#', a, b, c, d, e, dir, ..] => {
+                (parse_hex(dir, [a, b, c, d, e]), 14)
             }
-            &[.., a, b, c, d, e, dir, _] => {
-                parse_hex(dir, [a, b, c, d, e])
+            &[_, b' ', _, _, b' ', b'(', b'#', a, b, c, d, e, dir, ..] => {
+                (parse_hex(dir, [a, b, c, d, e]), 15)
             }
             _ => unreachable!(),
         };
-        (dir, amnt)
-    }) {
+        position += len;
         let last = *path.last().unwrap();
         let diff = match direction {
-            b'U' => (0, -amnt),
-            b'R' => (amnt, 0),
-            b'D' => (0, amnt),
-            b'L' => (-amnt, 0),
+            b'0' => (0, -amnt),
+            b'1' => (amnt, 0),
+            b'2' => (0, amnt),
+            b'3' => (-amnt, 0),
             _ => unreachable!(),
         };
         points_on_path += amnt;
-        path.push((last.0 + diff.0, last.1 + diff.1))
+        path.push((last.0 + diff.0, last.1 + diff.1));
     }
     solve_points(&path, points_on_path)
 }

c226e351a6d3f765511db29e32cf7e3883fbd5db
clubby789 <jamie@hill-daniel.co.uk>
2023-12-25 04:58:36 +0000

    day 24: improve z3 use

--- src/day24.rs
+++ src/day24.rs
@@ -80,13 +80,14 @@ pub fn part1() -> usize {
             let vel_b_x = Int::from_i64(&ctx, vel_b.x);
             let vel_b_y = Int::from_i64(&ctx, vel_b.y);
 
-            let time_a = Real::new_const(&ctx, "time_a");
-            let time_b = Real::new_const(&ctx, "time_b");
-            let line_a_x = Real::add(&ctx, &[&pos_a_x.to_real(), &vel_a_x.to_real().mul(&time_a)]);
-            let line_a_y = Real::add(&ctx, &[&pos_a_y.to_real(), &vel_a_y.to_real().mul(&time_a)]);
+            let time_a = Real::fresh_const(&ctx, "time_a");
+            let time_b = Real::fresh_const(&ctx, "time_b");
+            let line_a_x = pos_a_x.to_real() + vel_a_x.to_real() * &time_a;
+            let line_a_y = pos_a_y.to_real() + vel_a_y.to_real() * &time_a;
 
-            let line_b_x = Real::add(&ctx, &[&pos_b_x.to_real(), &vel_b_x.to_real().mul(&time_b)]);
-            let line_b_y = Real::add(&ctx, &[&pos_b_y.to_real(), &vel_b_y.to_real().mul(&time_b)]);
+
+            let line_b_x = pos_b_x.to_real() + vel_b_x.to_real() * &time_b;
+            let line_b_y = pos_b_y.to_real() + vel_b_y.to_real() * &time_b;
 
             solver.assert(&time_a.ge(&Real::from_real(&ctx, 0, 1)));
             solver.assert(&time_b.ge(&Real::from_real(&ctx, 0, 1)));
@@ -129,53 +130,42 @@ pub fn part2() -> usize {
     let cfg = z3::Config::new();
     let ctx = z3::Context::new(&cfg);
     let s = Solver::new(&ctx);
-    let p_x = Int::new_const(&ctx, "p_x");
-    let p_y = Int::new_const(&ctx, "p_y");
-    let p_z = Int::new_const(&ctx, "p_z");
-    let v_x = Int::new_const(&ctx, "v_x");
-    let v_y = Int::new_const(&ctx, "v_y");
-    let v_z = Int::new_const(&ctx, "v_z");
+
+    let p_x = Real::new_const(&ctx, "p_x");
+    let p_y = Real::new_const(&ctx, "p_y");
+    let p_z = Real::new_const(&ctx, "p_z");
+    let v_x = Real::new_const(&ctx, "v_x");
+    let v_y = Real::new_const(&ctx, "v_y");
+    let v_z = Real::new_const(&ctx, "v_z");
     // apparently solving for 3 is fine
     for (i, (pos, vel)) in hailstones.iter().enumerate().take(3) {
-        let time = Real::new_const(&ctx, format!("time_{i}"));
-        let thrown_x = Real::add(&ctx, &[&p_x.to_real(), &v_x.to_real().mul(&time)]);
-        let thrown_y = Real::add(&ctx, &[&p_y.to_real(), &v_y.to_real().mul(&time)]);
-        let thrown_z = Real::add(&ctx, &[&p_z.to_real(), &v_z.to_real().mul(&time)]);
+        let time = Real::fresh_const(&ctx, "time");
+        let thrown_x = &p_x + &v_x * &time;
+        let thrown_y = &p_y + &v_y * &time;
+        let thrown_z = &p_z + &v_z * &time;
 
-        let this_x = Real::add(
-            &ctx,
-            &[
-                &Real::from_int(&Int::from_i64(&ctx, pos.x)),
-                &Real::from_int(&Int::from_i64(&ctx, vel.x)).mul(&time),
-            ],
-        );
-        let this_y = Real::add(
-            &ctx,
-            &[
-                &Real::from_int(&Int::from_i64(&ctx, pos.y)),
-                &Real::from_int(&Int::from_i64(&ctx, vel.y)).mul(&time),
-            ],
-        );
-        let this_z = Real::add(
-            &ctx,
-            &[
-                &Real::from_int(&Int::from_i64(&ctx, pos.z)),
-                &Real::from_int(&Int::from_i64(&ctx, vel.z)).mul(&time),
-            ],
-        );
+        let this_x =
+            Int::from_i64(&ctx, pos.x).to_real() + Int::from_i64(&ctx, vel.x).to_real() * &time;
+        let this_y =
+            Int::from_i64(&ctx, pos.y).to_real() + Int::from_i64(&ctx, vel.y).to_real() * &time;
+        let this_z =
+            Int::from_i64(&ctx, pos.z).to_real() + Int::from_i64(&ctx, vel.z).to_real() * &time;
 
         s.assert(&thrown_x._eq(&this_x));
         s.assert(&thrown_y._eq(&this_y));
         s.assert(&thrown_z._eq(&this_z));
     }
-    println!("checking");
     if let SatResult::Sat = s.check() {
-        println!("checked");
         let m = s.get_model().unwrap();
-        let x = m.eval(&p_x, true).unwrap().as_i64().unwrap();
-        let y = m.eval(&p_y, true).unwrap().as_i64().unwrap();
-        let z = m.eval(&p_z, true).unwrap().as_i64().unwrap();
-        (x + y + z) as usize
+        [p_x, p_y, p_z]
+            .iter()
+            .map(|p| {
+                let Some((n, 1)) = m.get_const_interp(p).and_then(|v| v.as_real()) else {
+                    unreachable!();
+                };
+                n
+            })
+            .sum::<i64>() as usize
     } else {
         panic!("unsat")
     }
